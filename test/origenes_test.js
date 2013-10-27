fb15db1389555fe02109d80d6aaec6bb3a8dc052
Patrick De Marta <patrick.demarta@gmail.com>
2013-10-27 02:53:31 +0100

    add reverse, reverseComplement

--- test/origenes_test.js
+++ test/origenes_test.js
@@ -3,8 +3,18 @@
 var origenes = require('../lib/origenes.js');
 
 exports['test'] = {
-	'complement DNA squence' : function(test) {
+	'complement a DNA squence' : function(test) {
 		test.equal(origenes.complement("ACTG"), "TGAC");
 		test.done();
 	},
+	
+	'reverse a DNA squence' : function(test) {
+		test.equal(origenes.reverse("ACTG"), "GTCA");
+		test.done();
+	},
+	
+	'reverse and complement a DNA sequence' : function(test) {
+		test.equal(origenes.reverseComplement("ACTG"), "CAGT");
+		test.done();
+	}
 };
