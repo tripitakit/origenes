8641c164ccb0b59a0530f0c244d87dcf76831c5d
Patrick De Marta <patrick.demarta@gmail.com>
2013-10-27 15:44:13 +0100

    adding R/Y base complement, default N, drop  "?"

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Origenes
 ===
 Implementing some basic nucleotide's sequences manipulation tools.
-My first self-study module to learn node; this is an early alpha release, of no practical public use.
+Self-study module to learn node. Early alpha release, of no practical public use.
 
 
 
@@ -10,13 +10,25 @@ My first self-study module to learn node; this is an early alpha release, of no 
 Install the module with: `npm install origenes`
 
 ```javascript
-var origenes = require('origenes');
+var Sequence = require('origenes').Sequence;
+	
+// Create sequence instances
+var simple_oligo = new Sequence('ACTG'),
+	complex_oligo = new Sequence('AN-RCTYGK');
+	
+// Properties	
+simple_oligo.type; // 'DNA'
+simple_oligo.seq; // 'ACTG'
+simple_oligo.len; // 4
+complex_oligo.len; // 9
 
-origenes.complement("AGTC"); // "TCAG"
+// Methods
+simple_oligo.reverse(); // 'GTCA'
+simple_oligo.complement();  // 'TGAC'
+complex_oligo.complement(); // 'TN-YGARCN'
+simple_oligo.reverseComplement(); // 'CAGT'
 
-origenes.reverse("AGTC"); // "CTGA"
 
-origenes.reverseComplement("AGTC"); // "GACT" 
 ```
 
 ## Documentation
@@ -29,7 +41,8 @@ _(Coming soon)_
 In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
 
 ## Release History
-_(Nothing yet)_
+0.0.6 Am I serious? :>
+0.0.1 - 0.0.5 Just playing around
 
 ## License
 Copyright (c) 2013 tripitakit  
--- test/origenes_test.js
+++ test/origenes_test.js
@@ -1,20 +1,42 @@
-'use strict';
+'use strict'
 
-var origenes = require('../lib/origenes.js');
+var Sequence = require('../lib/origenes').Sequence;
 
 exports['test'] = {
-	'complement a DNA squence' : function(test) {
-		test.equal(origenes.complement("ACTG"), "TGAC");
+	
+    setUp: function(done) {
+      this.simple_oligo = new Sequence("ACTG");
+	  this.complex_oligo = new Sequence("AN-RCTYGK");
+      done();
+    },
+	
+	'create a DNA sequence' : function(test) {
+		test.expect(4);
+		test.strictEqual(this.simple_oligo.type, 'DNA');
+		test.strictEqual(this.simple_oligo.seq, "ACTG")
+		test.strictEqual(this.simple_oligo.len,4);
+		test.strictEqual(this.complex_oligo.len,9);
 		test.done();
 	},
 	
-	'reverse a DNA squence' : function(test) {
-		test.equal(origenes.reverse("ACTG"), "GTCA");
+	'complement a sequence' : function(test) {
+		test.strictEqual(this.simple_oligo.complement(), "TGAC");
 		test.done();
 	},
 	
-	'reverse and complement a DNA sequence' : function(test) {
-		test.equal(origenes.reverseComplement("ACTG"), "CAGT");
+	'complement a sequence with N, gaps and unrecognized nucleotides' : function(test) {
+		test.strictEqual(this.complex_oligo.complement(), "TN-YGARCN");
 		test.done();
 	},
+	
+	'reverse a sequence' : function(test) {
+		test.strictEqual(this.simple_oligo.reverse(), "GTCA");
+		test.done();
+	},
+	
+	'reverse and complement a sequence' : function(test) {
+		test.strictEqual(this.simple_oligo.reverseComplement(), "CAGT");
+		test.done();
+	},
+		
 };
